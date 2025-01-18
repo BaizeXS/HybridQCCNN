7f8eada1f8a006df5e295a1b49a6428bc800de06
BaizeXS <baizexs@gmail.com>
2025-01-18 18:15:08 +0800

    Modified SimpleVGG and HybridVGG code. Add examples and tests code.

---
+++ src/models/vgg/__init__.py
@@ -0,0 +1,7 @@
+from .hybrid_vgg import HybridVGG
+from .simple_vgg import SimpleVGG
+
+__all__ = [
+    'SimpleVGG',
+    'HybridVGG'
+]
---
+++ src/models/vgg/hybrid_vgg.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+from components.quanv import Quanv2d
+
+
+class HybridVGG(nn.Module):
+
+    def __init__(self, num_classes=10, **kwargs):
+        super(HybridVGG, self).__init__()
+        self.features = nn.Sequential(
+            # Layer 1: Classical Convolution
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Layer 2: Quantum Convolution
+            # TODO: Reduce the dimension first and then increase the dimension
+            nn.Conv2d(32, 3, kernel_size=1, stride=1, padding=0),
+            Quanv2d(3, 64, kernel_size=2, stride=1, padding=1, **kwargs),
+            # No need to introduce ReLU here, quantum convolution has already introduced nonlinearity.
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Layer 3: Classical Convolution
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((4, 4))
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 4 * 4, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(128, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
